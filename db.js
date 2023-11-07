c73f2973b03500b89fd214fdab6362572feb54e9
CAPSLOCK STUDIOS <135025637+Capslock-Studios@users.noreply.github.com>
2023-11-07 18:26:01 +0200

    With Profilep-App Attched
    User profile active but hidden
    Test Deplopy

---
+++ db.js
@@ -0,0 +1,25 @@
+<script>
+$userID
+    const map() {
+        Id(INT), Parent(user), Child(location){
+          
+        if Parent => 1
+        {}
+        echo ('user'+ 'location');
+
+        else echo('<p>No Registered Photographers in the area</p><br><a  href="refister.html">register yourself</a>');
+
+        if else end;
+        }
+    }
+</script>
+
+//{search_criteria} - By location [Jhb, Kagiso, East Gate]
+                // - 
+                //
+                //
+                ///----
+                //Tkae Mthungathi
+                //Shaun Khoza
+                //--
+                //
